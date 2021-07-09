cbf616a0471cbac3e31d78bdca5f7737218c88a5
Dmitry Skokov <d.skokov@rbkmoney.com>
2021-07-09 18:51:32 +0300

    first commit

---
+++ config/payouter/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ue
+
+java \
+    "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+-jar /opt/payouter/payouter.jar \
+--logging.file=/var/log/payouter/payouter.json \
+--logging.config=/opt/payouter/logback.xml \
+--management.security.enabled=false \
+-Dwoody.node_id=346 \
+--service.dominant.url=http://dominant:8022/v1/domain/repository_client \
+--service.shumway.url=http://shumway:8022/shumpune \
+--service.partyManagement.url=http://hellgate:8022/v1/processing/partymgmt \
+--service.fistful.url=http://fistful:8022/v1/admin \
+--service.fistful.sourceId={{ service['service']['fistful']['sourceId'] }} \
+--kafka.bootstrap-servers=kafka:9092 \
+--kafka.topics.party-management.id=mg-events-party \
+--kafka.topics.invoice.enabled=false \
+--kafka.topics.party-management.enabled=false \
+--kafka.topics.party-management.concurrency=5 \
+--kafka.client-id=payouter \
+--kafka.consumer.group-id=payouter-invoicing \
+--kafka.consumer.concurrency=5 \
+--kafka.consumer.auto-offset-reset=latest \
+${@} \
+--spring.config.additional-location=/vault/secrets/application.properties
